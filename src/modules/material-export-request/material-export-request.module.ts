2d43f8ec6e9f67368c63f3b7db05fc99a290562d
PhiNSE <93396482+PhiNSE@users.noreply.github.com>
2024-12-05 21:40:48 +0700

    hotfix: conflict

--- src/modules/material-export-request/material-export-request.module.ts
+++ src/modules/material-export-request/material-export-request.module.ts
@@ -3,12 +3,11 @@ import { PrismaModule } from 'prisma/prisma.module';
 import { ChatModule } from '../chat/chat.module';
 import { DiscussionModule } from '../discussion/discussion.module';
 import { MaterialExportReceiptModule } from '../material-export-receipt/material-export-receipt.module';
+import { ProductionBatchModule } from '../production-batch/production-batch.module';
 import { TaskModule } from '../task/task.module';
 import { MaterialExportRequestController } from './material-export-request.controller';
 import { MaterialExportRequestService } from './material-export-request.service';
 import { IsMaterialExportRequestExistValidator } from './validator/is-material-export-request-exist.validator';
-import { ChatModule } from '../chat/chat.module';
-import { ProductionBatchModule } from '../production-batch/production-batch.module';
 
 @Module({
   imports: [
